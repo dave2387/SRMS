2765810683c53d0bc226fec836c31bc3e9a4a528
dave2387 <davedhruv863@gmail.com>
2026-03-11 13:19:44 +0530

    UI

---
+++ src/app/demo2/page.tsx
@@ -0,0 +1,15 @@
+export default function Page() {
+
+  async function handleSubmit(formData:FormData) {
+    "use server"
+
+    console.log(formData.get("name"))
+  }
+
+  return (
+    <form action={handleSubmit}>
+      <input type="text" name="name" />
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
